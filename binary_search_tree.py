d592d265f0002bb2fb3e1cc79c52c7142b133792
Rabbi Hasan <jasrabbi50@gmail.com>
2024-06-26 22:41:46 +0600

    find max depth with recursion

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -200,6 +200,24 @@ class Tree:
         rootToLeaf(self.root, [], find_paths)
         
         return find_paths
+    
+    # find max depth
+    
+    def max_depth_recursion(self):
+        
+        def max_depth(node):
+            if node is None:
+                return 0
+            else:
+                left_depth = max_depth(node.left)
+                right_depth = max_depth(node.right)
+            
+            return max(left_depth, right_depth) + 1
+        
+        return max_depth(self.root)
+    
+    
+    
         
         
     
@@ -250,5 +268,7 @@ print('print all root to path')
 # tree.rootToLeaf()
 
 print(tree.find_paths_using_preorder())
+
+print('Max depth:', tree.max_depth_recursion())
 
 
